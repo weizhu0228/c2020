ba82faf1f36d4de5920b9ec316a20fb85a4ce5d9
weizhu0228 <74294675+weizhu0228@users.noreply.github.com>
2020-11-18 09:11:22 +0800

    Create 111801.c

---
+++ 111801.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int a=3,b=4,c=5,x,y;
+if(a+b>c&&b==c){
+printf("a为真命题\n");
+}
+else if(a||b+c&&b-c)
+{
+printf("a为真命题\n");
+}
+else if(!(a>b)&&!c||1)
+{
+printf("a为真命题\n");
+}
+else if(!(x=a)&&(y=b)&&0)
+{
+printf("a为真命题\n");
+}
+else if(!(a+b)+c-1&&b+c/2)
+{
+printf("a为真命题\n");
+}
+else
+{
+printf("a为假命题\n");
+}
