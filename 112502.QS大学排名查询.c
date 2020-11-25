306776b7a61e1a2f0523203db64348d7b2c8ac5e
weizhu0228 <74294675+weizhu0228@users.noreply.github.com>
2020-11-25 17:01:52 +0800

    Update 112502.QS大学排名查询.c

--- 112502.QS大学排名查询.c
+++ 112502.QS大学排名查询.c
@@ -6,43 +6,43 @@ printf("QS大学排名查询\n");
 scanf("%d",&i);
 if(i==1)
 {
-printf("清华大学\n");
+printf("麻省理工学院\n");
 }
 else if(i==2)
 {
-printf("北京大学\n");
+printf("斯坦福大学\n");
 }
 else if(i==3)
 {
-printf("中国科学技术大学\n");
+printf("哈佛大学\n");
 }
 else if(i==4)
 {
-printf("浙江大学\n");
+printf("牛津大学\n");
 }
 else if(i==5)
 {
-printf("复旦大学\n");
+printf("加州理工学院\n");
 }
 else if(i==6)
 {
-printf("南京大学\n");
+printf("苏黎世联邦工业大学\n");
 }
 else if(i==7)
 {
-printf("上海交通大学\n");
+printf("剑桥大学\n");
 }
 else if(i==8)
 {
-printf("中山大学\n");
+printf("伦敦大学学院\n");
 }
 else if(i==9)
 {
-printf("北京师范大学\n");
+printf("帝国理工学院\n");
 }
 else if(i==10)
 {
-printf("华中科技大学\n");
+printf("芝加哥大学\n");
 }
 else
 {
