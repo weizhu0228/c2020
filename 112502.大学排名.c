f278aa2420f99c42ec75cc0d458b8e87dfe420c8
weizhu0228 <74294675+weizhu0228@users.noreply.github.com>
2020-11-25 09:49:52 +0800

    Create 112502.大学排名.c

---
+++ 112502.大学排名.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void main()
+{
+int i;
+printf("QS大学排名查询\n");
+scanf("%d",&i);
+if(i==1)
+{
+printf("清华大学\n");
+}
+else if(i==2)
+{
+printf("北京大学\n");
+}
+else if(i==3)
+{
+printf("中国科学技术大学\n");
+}
+else if(i==4)
+{
+printf("浙江大学\n");
+}
+else if(i==5)
+{
+printf("复旦大学\n");
+}
+else if(i==6)
+{
+printf("南京大学\n");
+}
+else if(i==7)
+{
+printf("上海交通大学\n");
+}
+else if(i==8)
+{
+printf("中山大学\n");
+}
+else if(i==9)
+{
+printf("北京师范大学\n");
+}
+else if(i==10)
+{
+printf("华中科技大学\n");
+}
+else
+{
+printf("老铁的排名太靠后了，请输入");
+}
+}
