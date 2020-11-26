527cc1c5ce8365697b3bf81a9be5cd9a9d41bc71
weizhu0228 <74294675+weizhu0228@users.noreply.github.com>
2020-11-26 18:52:10 +0800

    Update 112505.猜数字.c

--- 112505.猜数字.c
+++ 112505.猜数字.c
@@ -2,19 +2,25 @@
 #include<windows.h>
 void main()
 {
-	int daan=10,number;
-	scanf("%d",&number);
-	if(number==daan)
-	{
-	printf("恭喜你，挑战成功!!!\n");
-	}
-    else if(number>daan)
-	{
-	printf("你猜的太大了。\n");
-	}
-	else
-	{
-	printf("你猜的太小了。\n");
-	}
-	
+int daan=10,a=1,i;
+printf("猜数字,请输入1——10\n");
+while(a<=10)
+{
+printf("第%d次挑战\n",a);
+scanf("%d",&i);
+if(i==daan)
+{
+printf("恭喜你，猜对了！！！");
+a=a+10;
+}
+else if(i>daan)
+{
+printf("你的答案猜大了。");
+}
+else
+{
+printf("你的答案猜小了。");
+a++;
+}
+}
 }
