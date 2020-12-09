462f1d6c6aec3bfbf01506bb8b902769afd3326b
weizhu0228 <74294675+weizhu0228@users.noreply.github.com>
2020-12-09 17:06:13 +0800

    Create 120906.字符长度.c

---
+++ 120906.字符长度.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+	char a1[]="fall in love with sb at first sight";
+	char a2[]="一见钟情";
+	printf("%d",sizeof(a1));
+	printf("\n");
+	printf("%d",sizeof(a2));
+}
